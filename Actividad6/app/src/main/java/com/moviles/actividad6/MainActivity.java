b0629bdfb09bfaa3484cabd85930e3a33775dc13
Manuersito <manuelbc03@hotmail.com>
2024-10-07 09:34:58 +0200

    Terminado

---
+++ Actividad6/app/src/main/java/com/moviles/actividad6/MainActivity.java
@@ -0,0 +1,42 @@
+package com.moviles.actividad6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void pasar(View view){
+        Intent intent = new Intent(MainActivity.this, RecibeRespuesta.class);
+
+        String cadena = "hola mundo!";
+        double decimal = 24.25;
+
+        intent.putExtra("Cadena",cadena);
+        intent.putExtra("Decimal",decimal);
+
+        startActivity(intent);
+
+    }
+
+
+
+}
---
+++ Actividad6/app/src/main/java/com/moviles/actividad6/RecibeRespuesta.java
@@ -0,0 +1,37 @@
+package com.moviles.actividad6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RecibeRespuesta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recibe_respuesta);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+    public void mostrar(View view){
+        Intent intent = getIntent();
+        String cadena = intent.getStringExtra("Cadena");
+        double decimal = intent.getDoubleExtra("Decimal",0.0);
+
+        Toast.makeText(this, "Cadena"+cadena+", decimal "+decimal, Toast.LENGTH_SHORT).show();
+
+    }
+}
