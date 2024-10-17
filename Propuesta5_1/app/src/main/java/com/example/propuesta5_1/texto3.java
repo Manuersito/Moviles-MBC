7d20b416a7df2056a8f2a53f637063ad38cacb85
Manuersito <manuelbc03@hotmail.com>
2024-10-17 10:25:07 +0200

    Terminado

--- Propuesta5_1/app/src/main/java/com/example/propuesta5_1/texto3.java
+++ Propuesta5_1/app/src/main/java/com/example/propuesta5_1/texto3.java
@@ -1,8 +1,9 @@
 package com.example.propuesta5_1;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,41 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class texto3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_texto3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.maintexto3), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView texto3 = (TextView) findViewById(R.id.texto3);
+        texto3.append("\n Texto añadido con append desde java");
+        texto3.setTextColor(Color.BLUE);
     }
 
-    public void texto1(View view){
-        Intent intent = new Intent(this, texto1.class);
+    public void onPause(){
+        super.onPause();
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
-    }
-
-    public void texto2(View view){
-        Intent intent = new Intent(this, texto2.class);
-        startActivity(intent);
-
-    }
-
-    public void texto3(View view){
-        Intent intent = new Intent(this, texto3.class);
-        startActivity(intent);
-
-    }
-
-    public void texto4(View view){
-        Intent intent = new Intent(this, texto4.class);
-        startActivity(intent);
-
     }
 }
--- Propuesta5_1/app/src/main/java/com/example/propuesta5_1/texto4.java
+++ Propuesta5_1/app/src/main/java/com/example/propuesta5_1/texto4.java
@@ -1,8 +1,10 @@
 package com.example.propuesta5_1;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,41 +12,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class texto4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_texto4);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.maintexto4   ), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView texto4 = (TextView) findViewById(R.id.texto4);
+        Typeface mifuente = Typeface.createFromAsset(getAssets(),"fonts/Umbrella.ttf");
+        texto4.setTypeface(mifuente);
     }
 
-    public void texto1(View view){
-        Intent intent = new Intent(this, texto1.class);
+    public void onPause(){
+        super.onPause();
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
-    }
-
-    public void texto2(View view){
-        Intent intent = new Intent(this, texto2.class);
-        startActivity(intent);
-
-    }
-
-    public void texto3(View view){
-        Intent intent = new Intent(this, texto3.class);
-        startActivity(intent);
-
-    }
-
-    public void texto4(View view){
-        Intent intent = new Intent(this, texto4.class);
-        startActivity(intent);
-
     }
 }
