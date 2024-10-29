1f9aeef853d577962e76610727bac7c6dc89af70
Manuersito <manuelbc03@hotmail.com>
2024-10-29 13:35:05 +0100

    hacer wordle

---
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.listview;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ListView listado = (ListView) findViewById(R.id.lista);
+        final String[] datos = new String[]{ "Elemento1","Elemento2","Elemento3","Elemento4", "Elemento5" };
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this , android.R.layout.simple_list_item_1, datos);
+        listado.setAdapter(adaptador);
+
+
+    }
+
+
+
+
+
+
+}
+
