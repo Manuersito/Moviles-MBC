8c04053efd05015f2e05193c5459f26bbbb1e4d6
Manuersito <manuelbc03@hotmail.com>
2025-01-14 14:42:30 +0100

    t

--- Trimestre2/Actividad9_4/app/src/main/java/com/example/actividad9_4/Fragmento1.java
+++ Trimestre2/Actividad9_4/app/src/main/java/com/example/actividad9_4/Fragmento1.java
@@ -1,18 +1,31 @@
 package com.example.actividad9_4;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
 
 public class Fragmento1 extends ListFragment {
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Configuración del adaptador
+        setListAdapter(new Adaptador(requireActivity(), R.layout.layout_listado, Contenido.ENT_LISTA) {
+            @Override
+            public void onEntrada(Object entrada, View view) {
+                // Configuramos el texto
+                TextView texto_superior_entrada = view.findViewById(R.id.textotitulo);
+                texto_superior_entrada.setText(((Contenido.Lista_entrada) entrada).textoEncima);
+
+                // Configuramos la imagen
+                ImageView imagen_entrada = view.findViewById(R.id.imagenlista);
+                imagen_entrada.setImageResource(((Contenido.Lista_entrada) entrada).idImagen);
+            }
+        });
     }
 }
